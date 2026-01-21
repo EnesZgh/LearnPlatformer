dd09730e293d3fbfa1cd7609ffc9e1fdc3d02473
EnesZgh <134294333+EnesZgh@users.noreply.github.com>
2026-01-21 16:02:49 +0100

    Mon projet complet

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,59 @@
+using System.Xml;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public float invicibilityTimeAfterHit = 3f;
+    public float invicibilityFlashDelay = 0.1f;
+    public bool isInvicible = false;
+
+    public SpriteRenderer graphics;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!isInvicible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            isInvicible=true;
+            StartCoroutine(InvicibilityFlash());
+            StartCoroutine(HandleInvicibilityDelay());
+        }
+    } 
+
+    public IEnumerator InvicibilityFlash()
+    {
+        while (isInvicible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(invicibilityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(invicibilityFlashDelay);
+        }
+    }
+
+    public IEnumerator HandleInvicibilityDelay()
+    {
+        yield return new WaitForSeconds(invicibilityTimeAfterHit);
+        isInvicible = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
+
+
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+
+        Flip(rb.linearVelocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetFloat("Speed", characterVelocity);
+    }
+    void FixedUpdate()
+    {
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, .05f);
+
+        if(isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping=false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if(_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
